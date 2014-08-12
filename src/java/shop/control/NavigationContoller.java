ead9fa901b13c766886a63310d54f8af4ee22764
Jean claude <coco@my-wi-fi.local>
2014-08-11 20:17:44 -0500

    Hibernate configuration
    Done hibernate configuration

--- src/java/shop/control/NavigationContoller.java
+++ src/java/shop/control/NavigationContoller.java
@@ -3,19 +3,25 @@ package shop.control;
 
 import java.io.Serializable;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import shop.dao.IProductDao;
+import shop.dao.ProductDao;
 
 
-
-@Named("navcontrol")
+@ManagedBean(name="navcontrol")
 @ApplicationScoped
 public class NavigationContoller  implements Serializable{
+    private static final String login = "login";
     private static final String browser_products = "browseProducts";
-
+    private IProductDao productDao;
     public NavigationContoller() {
     }
-    
+    public String checkLogin(){
+        return login;
+    }
     public String browseProducts(){
+        productDao = new ProductDao();
+        productDao.getProducts();
         return browser_products;
     }
     public String addProduct(){
--- src/java/shop/control/ProductController.java
+++ src/java/shop/control/ProductController.java
@@ -1,14 +1,37 @@
 
 package shop.control;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import shop.dao.IProductDao;
+import shop.dao.ProductDao;
+import shop.entity.Product;
 
-@Named
-@RequestScoped
-public class ProductController {
-
+@ManagedBean
+@SessionScoped
+public class ProductController implements Serializable{
+    private static final String browser_products = "browseProducts";
+    private static final String add_product = "addProduct";
+    private IProductDao productDao;
+    private List<Product> productList;
+    
     public ProductController() {
     }
+    public String addProduct(){
+        return add_product;
+    }
+    public String browseProducts(){
+        productDao = new ProductDao();
+        productList = (List<Product>) productDao.getProducts();
+        System.out.println(" In get product list "+productList.size());
+        return browser_products;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+    
     
 }
